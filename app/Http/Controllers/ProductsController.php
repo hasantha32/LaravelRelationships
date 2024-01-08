59ec22e68ddc8810193b23f6a7ae461c8b5433b9
hasantha32 <hasanthamadushan32@gmail.com>
2024-01-08 13:23:28 +0530

    added transaction for save method

--- app/Http/Controllers/ProductsController.php
+++ app/Http/Controllers/ProductsController.php
@@ -7,27 +7,36 @@ use App\Http\Requests\ProductsRequest;
 use App\Models\Photos;
 use App\Models\Products;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class ProductsController extends Controller
 {
     //
     public function save(ProductsRequest $request)
     {
-        $items = new Products();
-        $items->name = $request->name;
-        $items->price = $request->price;
-        $items->quantity = $request->quantity;
-        $items->save();
+        try {
+            DB::beginTransaction();
+            $items = new Products();
+            $items->name = $request->name;
+            $items->price = $request->price;
+            $items->quantity = $request->quantity;
+            $items->save();
 
-        $imageName = $request->image->getClientOriginalName();
+            $imageName = $request->image->getClientOriginalName();
 
-        //store the photo in project folder
-        $request->image->move(public_path('images'),$imageName);
+            //store the photo in project folder
+            $request->image->move(public_path('images'),$imageName);
 
-        //save photo in the db
-        $photos = new Photos();
-        $photos->path = 'image/' .$imageName;
-        $photos->product_id = $items->id;
-        $photos->save();
+            //save photo in the db
+            $photos = new Photos();
+            $photos->path = 'image/' .$imageName;
+            $photos->product_id = $items->id;
+            $photos->save();
+            DB::commit();
+        }catch (\Exception $e){
+            DB::rollBack();
+    }
+
+
      }
 }
